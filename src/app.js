f7a21daa5d3607dccc01639f4dfa691dcb6c99c6
Jonathan Wong <Jonwyp@users.noreply.github.com>
2020-02-19 15:02:03 +0800

    Add Profile and env.example

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const cors = require("cors");
 const stocksRouter = require("./routes/stocks.routes");
 const usersRouter = require("./routes/users.route");
 
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:3001"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
